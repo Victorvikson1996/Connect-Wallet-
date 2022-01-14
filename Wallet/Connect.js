8d24ace8fe725997dac35d182e88b1c42888ca85
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2022-01-14 11:46:21 +0530

    Connect Setup

---
+++ Wallet/Connect.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+
+export const Connect = () => {
+  return (
+    <View>
+      <Text>Connect</Text>
+    </View>
+  )
+}
